84e88c77cdcf61b6bc1b6aad4989b9e959b12c9d
RedBeard <redbeard.eth@protonmail.com>
2021-09-13 14:16:20 +1000

    remove async image gen

--- checkSales.ts
+++ checkSales.ts
@@ -5,19 +5,34 @@ import { ethers } from "ethers";
 import fs from "pn/fs"
 import buildSaleHTML from "./buildSaleHtml"
 import nodeHtmlToImage from 'node-html-to-image';
+import { deepStrictEqual } from 'node:assert';
+
+const { DISCORD_BOT_TOKEN, DISCORD_CHANNEL_ID, NODE_ENV = 'development' } = process.env;
+if (!DISCORD_CHANNEL_ID || !DISCORD_BOT_TOKEN) {
+  throw new Error('MISSING REQUIRED ENV VARIABLES');
+}
 
 const OPENSEA_SHARED_STOREFRONT_ADDRESS = '0x495f947276749Ce646f68AC8c248420045cb7b5e';
 
-const discordBot = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const intents = new Intents();
+intents.add(
+  Intents.FLAGS.GUILDS,
+ /* Intents.FLAGS.GUILD_MEMBERS,
+  Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+  Intents.FLAGS.GUILD_PRESENCES,*/
+  Intents.FLAGS.GUILD_MESSAGES,
+  /*Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  Intents.FLAGS.DIRECT_MESSAGES,
+  Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,*/
+);
+
+const discordBot = new Discord.Client({ intents });
 const  discordSetup = async (): Promise<TextChannel> => {
   return new Promise<TextChannel>((resolve, reject) => {
-    ['DISCORD_BOT_TOKEN', 'DISCORD_CHANNEL_ID'].forEach((envVar) => {
-      if (!process.env[envVar]) reject(`${envVar} not set`)
-    })
-  
-    discordBot.login(process.env.DISCORD_BOT_TOKEN);
+
+    discordBot.login(DISCORD_BOT_TOKEN);
     discordBot.on('ready', async () => {
-      const channel = await discordBot.channels.fetch(process.env.DISCORD_CHANNEL_ID!);
+      const channel = await discordBot.channels.fetch(DISCORD_CHANNEL_ID!);
       resolve(channel as TextChannel);
     });
   })
@@ -58,22 +73,21 @@ async function main() {
   const openSeaResponse = await fetch(
     "https://api.opensea.io/api/v1/events?" + params).then((resp) => resp.json());
     
-  return await Promise.all(
-    openSeaResponse?.asset_events?.reverse().map(async (sale: any) => {
+  const assets = openSeaResponse?.asset_events.reverse()
 
-      const openSeaResponse = await fetch(
-        "https://api.opensea.io/api/v1/asset/0x7afe30cb3e53dba6801aa0ea647a0ecea7cbe18d/" + sale.asset.token_id).then((resp) => resp.json());
-
+  const sendEmbed = async(sale) => {
+      console.log('sending ' + sale.asset.name)
+      const openSeaResponse = await fetch("https://api.opensea.io/api/v1/asset/0x7afe30cb3e53dba6801aa0ea647a0ecea7cbe18d/" + sale.asset.token_id)
+      const imageJson = await openSeaResponse.json()
       const image = await nodeHtmlToImage({
-        html: buildSaleHTML(sale, openSeaResponse),
+        html: buildSaleHTML(sale, imageJson),
         quality: 100,
         type: 'jpeg',
         puppeteerArgs: {
           args: ['--no-sandbox', '--disable-setuid-sandbox'],
         }
       })
-      await fs.writeFileSync('realm.jpeg', image);
-
+      const imagewrite = fs.writeFileSync('realm.jpeg', image);
       const embed = new Discord.MessageEmbed()
         .setTitle('🗺️ New Realm Sale')
         .setImage("attachment://realm.jpeg")        
@@ -89,19 +103,31 @@ async function main() {
             inline: true
           },
         );
-      return channel.send({ embeds: [embed], files: [buildImage(image)] });
-
+       const message = await channel.send({ embeds: [embed], files: [buildImage(image)] });
+          return message
      /* const message = buildMessage(image);
       const links = buildLinks(sale);
       channel.send(message)
       return channel.send(links)*/
-    })
-  );   
+    
+    }; 
+    let embedResponse = []
+
+    for (let i = 0; i < assets.length; i++) {
+      const embedSent = await sendEmbed(assets[i])
+      embedResponse.push(embedSent)
+    }
+    console.log(embedResponse)
+    return embedResponse
+  
 }
 
 main()
   .then((res) =>{ 
-    if (!res.length) console.log("No recent sales")
+    if (!res.length) {
+      console.log('No Sales')
+    }
+    console.log('end')
     process.exit(0)
   })
   .catch(error => {
